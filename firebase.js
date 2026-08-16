b6c3ca38a2f68b4d7c996e6a8d8d7a655e6a39a0
gooddaylineoa <goodday.lineoa@gmail.com>
2026-08-16 18:33:45 +0700

    เริ่มต้นโปรเจกต์

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBewLVeVkpe8Bh5Gm0RuOVAp27aHFkPgQ8",
+  authDomain: "goodday-members.firebaseapp.com",
+  projectId: "goodday-members",
+  storageBucket: "goodday-members.firebasestorage.app",
+  messagingSenderId: "30572263082",
+  appId: "1:30572263082:web:4a77979e77c3b4f49c82d3",
+  measurementId: "G-HDR86BDFL3"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
